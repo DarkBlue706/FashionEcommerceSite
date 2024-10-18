a3e9076921d1948d567868b723544f622b8eac68
DarkBlue706 <cthompson706@yahoo.com>
2024-10-18 16:12:50 -0400

    MoreFrontEndnCart5

--- obj/Debug/net8.0/ShirtCompany.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShirtCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShirtCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83cf8790f738b63f2f6fead7e9468857f2ad872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b5028bf5b71bdccbd9c19b6d224c114ec02b377")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShirtCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShirtCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
